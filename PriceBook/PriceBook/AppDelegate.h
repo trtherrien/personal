d909f1a5692f27c6c17f4e741b501d64cc7bbad2
Tom Therrien <trtherrien@icloud.com>
2013-02-06 22:23:16 -0500

    Initial commit

---
+++ PriceBook/PriceBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PriceBook
+//
+//  Created by Thomas Therrien on 2/6/13.
+//  Copyright (c) 2013 Thomas Therrien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PriceBook/PriceBook/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  PriceBook
+//
+//  Created by Thomas Therrien on 2/6/13.
+//  Copyright (c) 2013 Thomas Therrien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
